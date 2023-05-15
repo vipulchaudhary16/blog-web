42ae94942f5d5df1a4b98608c83d0705237838e3
vipulchaudhary16 <vipulr6111@gmail.com>
2023-05-15 19:11:09 +0530

    resolving cors issue

--- server/index.js
+++ server/index.js
@@ -1,17 +1,21 @@
 const express = require('express')
-const cors = require('cors')
 const dotenv = require('dotenv')
 dotenv.config()
 const app = express()
 const PORT = 8080 || process.env.PORT
 //for cross orgin request
-app.use(cors())
+const cors = require('cors');
+app.use(cors());
 
 //database connection
 require('./configDB')
 
 //request body will contains json data
 app.use(express.json())
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Anime blog backend API" })
+})
 
 //adding routes
 app.use("/api/auth", require("./routes/Auth"))
